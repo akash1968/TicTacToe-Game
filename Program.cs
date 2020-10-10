4251f74ba277c60b099cbd40a5c0f84f39295733
Akash <akash.singh670@gmail.com>
2020-10-10 19:00:30 +0530

    [Akash] .Added UC1 Of Tic Tac Toe Game

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Tic_Tac_Toe_Workshop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+             char[] board = createBoard();
+        }
+        //UC1
+        public static char[] createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';               
+            }
+            return board;
+        }
+    }
+}
